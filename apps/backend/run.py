1428890fecb49f00206fce4bb726f2b358373c27
rachit404 <doshirachit28@gmail.com>
2026-08-30 23:39:26 +0530

    feat: update server port to 8000 and remove extension manifest file

--- apps/backend/run.py
+++ apps/backend/run.py
@@ -8,7 +8,7 @@ app = create_app()
 if __name__ == "__main__":
     app.run(
         host="127.0.0.1",
-        port=5000,
+        port=8000,
         debug=app.config["DEBUG"],
     )
     CORS(app)
